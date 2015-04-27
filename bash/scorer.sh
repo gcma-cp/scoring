f7af866a6ba38ddbcfeb4bacbd4b979b06d91d2c
Michael's Linux Environment <mike_bailey@verizon.net>
2015-04-27 19:48:34 -0400

    Add a basic rogue user checker that iterates through array and prints score

--- bash/scorer.sh
+++ bash/scorer.sh
@@ -1,3 +1,15 @@
-#!/bin/sh
-#Initialize
+#!/bin/bash
 
+#Array, static for now but should be imported from CSV in future
+rougeUsers=( Rogue1 Rogue2 Rogue3 )
+# Rogue user portion
+for rougeuser in "${rougeUsers[@]}"
+do
+	grep $rougeuser /etc/passwd &> /dev/null
+		if [ $? -ne 0 ];
+		then
+		echo $rougeuser deleted
+		let score=$score+1
+		fi
+done
+echo Score is $score"00"
--- python/scorer.py
+++ python/scorer.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python
 import os
 import stat
 from sys import argv
-rogueuser="Todd"
+	rogueuser="Todd"
 hashedpasswords = open("/etc/shadow")
 passwdfile = open("/etc/passwd")
 groups = open("/etc/group")
-if rogueuser in passwdfile:
-	print("Rogue user detected")
-else:
-	print("Rogue user deleted")
+for x in rogueusers:
+	if rogueuser in passwdfile:
+		print("Rogue user detected")
+	else:
+		print("Rogue user deleted")
 
